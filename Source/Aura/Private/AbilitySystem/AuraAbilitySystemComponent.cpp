fb34adc616b79d67f45a860933302f2622bf3e73
leavemydogalone <niklasport88@gmail.com>
2024-04-08 20:39:03 -0700

    added logic to effect applied to show tags

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -10,5 +10,12 @@ void UAuraAbilitySystemComponent::AbilityActorInfoSet()
 
 void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle)
 {
-	GEngine->AddOnScreenDebugMessage(1, 8.f, FColor::Blue ,FString("Effect Applied"));
+	FGameplayTagContainer TagContainer;
+	EffectSpec.GetAllAssetTags(TagContainer);
+	for (FGameplayTag Tag : TagContainer)
+	{
+		const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+		//TODO: broadcast the tag to the widget controller
+		GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);
+	}
 }
